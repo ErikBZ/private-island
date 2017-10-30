fe14c4f68585b3e242ab78abd28bbca207e27e4f
Brandon Zapata <zapatabrandon@gmail.com>
2017-10-30 11:38:46 -0400

    requests are not recived by the server. Generic respones can be sent but well crafted HTTP respones are not working and messages don't render on the brower

--- src/http.rs
+++ src/http.rs
@@ -2,12 +2,28 @@
 // let's not fuck it up
 use std::net::{TcpListener, TcpStream};
 use std::time::Duration;
+use std::io::Read;
+use std::io::Write;
+use std::str;
+// used for formatting strings
+use std::fmt;
 
-// does not 
-pub fn handle_http_request(mut stream: TcpStream){
+// since we borrow TcpStream we dont need to return it again to
+// pass ownership back
+// Reads the request from the tcpStream and returns it
+// since our returning string is not contained within TcpStream we cannot
+// return a borrowed "&str". We have to create a owner string and pass
+// ownership to the caller
+
+// syntax for borrowing a mutable borrow is make the var mut
+// and the borrow must be attached to the var type
+pub fn handle_http_request(mut stream: &TcpStream) -> String{
     // setting a 3 second timeout duration
+    // this won't work with telnet though
+    /*
     let dur = Duration::new(3, 0);
     stream.set_read_timeout(Some(dur)).unwrap();
+    */
 
     match stream.peer_addr(){
         Ok(addr) => {
@@ -19,13 +35,43 @@ pub fn handle_http_request(mut stream: TcpStream){
     }
 
     // allocating 1024 bytes to read the request
-    let buffer = allocate_vector(1024 as usize);
+    let mut buffer = allocate_vector(1024 as usize);
 
+    // dont need the assert this time around since i know it works
+    // actually just in case
+    assert_eq!(buffer.len(), 1024);
+
+    let request = match stream.read(&mut buffer){
+        Ok(size) => {
+            println!("Reading {} bytes into buffer\n",size);
+            // from_utf8 borrows the buffer
+            let s = match str::from_utf8(&buffer){
+                Ok(string) => string,
+                Err(e) => panic!("Buffer read but invalid encoding {}", e),
+            };
+            s
+        }
+        Err(e) =>{
+            panic!("Error occured while reading into buffer {}", e);
+        }
+    };
+
+    // converts &str to String
+    request.to_string()
+}
+
+// before this i'll probably need to parse the http request
+// to send the correct response
+pub fn send_http_response(mut stream: &TcpStream){
+    let html = "<h1>Hello World</h1>";
+    let response = format!("HTTP/1.0 200 OK\nContent-Type: text/html\nContent-Length: {}\n{}", html.len(), html);
+    stream.write(response.as_bytes()).unwrap();
+    println!("Response of size {} sent to peer", response.len());
 }
 
 fn allocate_vector(size:usize) -> Vec<u8>{
     let mut new_vec: Vec<u8> = Vec::with_capacity(size);
-    for i in 0..size{
+    for _i in 0..size{
         new_vec.push(0);
     }
     new_vec
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 // file that starts the server
-use std::net::SocketAddr;
 use std::net::{TcpListener, TcpStream};
 // because this wasn't in in scope, it would not let the
 // stream read bytes form the port
@@ -10,7 +9,6 @@ use std::time::Duration;
 use std::str;
 
 extern crate socket2;
-use socket2::{Domain, Socket, Type};
 
 // internal crate
 mod http;
@@ -18,10 +16,18 @@ mod http;
 fn main() {
     // it looks like somme ports under 1024 require special premission
     let listener = TcpListener::bind("127.0.0.1:8090").unwrap();
+
+    // all of the code has to be in this I guess
     for mut stream in listener.incoming(){
         match stream{
-            Ok(stream) => {
-                handle_client(stream);
+            Ok(mut stream_request) => {
+                http::handle_http_request(&stream_request);
+                // when returning result you have to unwrap or match
+                // i think write returns the number of bytes written to the stream
+                // YAY IT WORKS
+                // sending bytes works at least
+                // trying to correctly send http responses
+                http::send_http_response(&stream_request);
             }
             Err(_e) => {
                 println!("connection failed")
@@ -30,56 +36,9 @@ fn main() {
     }
 }
 
-// looks like stream has to be mutabe in order to actually read things in
-fn handle_client(mut stream: TcpStream) -> TcpStream{
-    println!("Connection all good");
-    let five_seconds = Duration::new(5, 0);
-    // for passingn in an option, you wrap the variable with a "Some" and parens not
-    // carrots
-    stream.set_read_timeout(Some(five_seconds)).unwrap();
-match stream.peer_addr(){
-        Ok(addr) =>{
-            println!("{:?}, reading from addr", addr);
-        }
-        Err(_e) => {
-            println!("No peer found");
-        }
-    }
-
-    // trying to send something back here
-    // it looks like the size of this buffer is only 0
-    // let mut buf: Vec<u8> = Vec::with_capacity(1024);
-
-    // with this allocation function buf read now
-    // reads bytes
-    let mut buf = alloc_vector(1024 as usize);
-    assert_eq!(buf.len(), 1024);
-
-
-    match stream.read(&mut buf){
-        Ok(size) => {
-            // it looks ike stream.read is only reading 0
-            // bytes for some reason.
-            println!("Size of buffer: {:?}", buf.len());
-            println!("Number of bytes read: {:?}", size);
-
-            // try to print out the request
-            // println!("\n{:?}\n", buf);
-            // it's probably best to call the function you just wrote
-            print_response(&buf)
-
-            // & for borrowing
-        }
-        Err(_e) =>{
-            println!("Oh no something went wrong");
-        }
-    }
-    stream
-}
-
 fn alloc_vector(size: usize) -> Vec<u8>{
     let mut vec : Vec<u8> = Vec::with_capacity(size);
-    for i in 0..size{
+    for _i in 0..size{
         vec.push(0);
     }
     vec
